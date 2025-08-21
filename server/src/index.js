f1d69c734e37d45318b62f3e296cda7eb31ee8d7
fra85h <francescog85@gmail.com>
2025-08-21 12:02:33 +0000

    modifica index.js

--- server/src/index.js
+++ server/src/index.js
@@ -2,7 +2,7 @@
 import express from 'express';
 import cors from 'cors';
 import { recomputeMatches, listMatches } from './models/matches.js';
-
+import 'dotenv/config';
 const app = express();
 app.use(cors());
 app.use(express.json());
